1d2216b98854a117aa218dd13bdf8d9d77ddd113
dongyoumuxi <983563524@qq.com>
2019-01-26 14:56:01 +0800

    add data_models.py to store two classes

--- data_manager/data_manager.py
+++ data_manager/data_manager.py
@@ -5,41 +5,10 @@
 # @Time    : 2019/1/25 16:44
 # @IDE     : PyCharm
 
-import xlrd
 import re
+import xlrd
 
-
-class MCM_NFLIS_Data:
-    """
-    store the MCM_NFLIS_Data
-    """
-
-    def state_converse(self, State):
-        return {
-            'KY': 'Kentucky',
-            'OH': 'Ohio',
-            'PA': 'Pennsylvania',
-            'VA': 'Virginia',
-            'WV': 'West Virginia'
-        }.get(State, 'error')
-
-    def __init__(self, data_list):
-        self.YYYY = data_list[0]
-        self.State = self.state_converse(data_list[1])
-        self.COUNTY = data_list[2]
-        self.FIPS_State = data_list[3]
-        self.FIPS_County = data_list[4]
-        self.FIPS_Combined = data_list[5]
-        self.SubstanceName = data_list[6]
-        self.DrugReports = data_list[7]
-        self.TotalDrugReportsCounty = data_list[8]
-        self.TotalDrugReportsState = data_list[9]
-
-
-class ACS_5YR_DP02:
-    def __init__(self, title_list, list):
-        for i in range(len(list)):
-            self.__setattr__(title_list[i], list[i])
+from data_models import MCM_NFLIS_Data, ACS_5YR_DP02
 
 
 def read_xlsx():
